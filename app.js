fa43bd852b2839cf27dff26eb663f281337d263b
maryanahermawan <maryanahermawan@gmail.com>
2018-09-01 16:36:04 +0800

    trying conflict with amanda

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/', routes);
-app.use('/toDos', toDosRouter);
+app.use('/todos', toDosRouter);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
